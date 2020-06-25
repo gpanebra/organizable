35f352f0a6a02afe6722afb91b6033100072a7c6
gpanebra <gpanebramoina@gmail.com>
2020-06-25 13:13:23 -0500

    update

--- assets/js/gustavo.js
+++ assets/js/gustavo.js
@@ -1,14 +1,3 @@
-/*var myBoardsButton = document.querySelector("my-boards-button");
-const closedBoardsButton = document.querySelector("closed-boards-button");
-const myProfileButton = document.querySelector("my-profile-button");
-const logOutButton = document.querySelector("log-out-button");*/
-
-/*function changeColor(element) {
-  element.style.color = "yellow";
-  element.stopPropagation();
-}
-*/
-//onclick="changeColor(this)"
 let sidebar = document.querySelector(".sidebar");
 let buttons = Array.from(sidebar.children);
 buttons.forEach((element) => element.addEventListener("click", navigation));
@@ -21,5 +10,3 @@ function navigation() {
   this.classList.add("unhoverable");
   this.classList.add("marked");
 }
-
-sidebar.addEventListener;
