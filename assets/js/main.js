8c92b57fae72558e5352fdaf238b8e3833021f45
Robert Medina <robertmedib1@gmail.com>
2020-06-25 02:58:25 -0500

    User can see his own profile an cad edit it too

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-import { login, signout, createUser } from "./robert.js";
-const user = JSON.parse(localStorage.getItem("user"));
+import { login, signout, createUser } from "./robert/sessions.js";
+import { show } from "./robert/profiles.js";
 
-console.log(user);
 const loginBtn = document.querySelector("#login-btn");
 const createBtn = document.querySelector("#create-btn");
 const signoutBtn = document.querySelector("#sign-out");
@@ -9,5 +8,3 @@ const signoutBtn = document.querySelector("#sign-out");
 if (createBtn) createBtn.addEventListener("click", createUser);
 if (loginBtn) loginBtn.addEventListener("click", login);
 if (signoutBtn) signoutBtn.addEventListener("click", signout);
-
-export { user };
