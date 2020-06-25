79abf045a26b57ca877bdfe0fbb5355e33e08bde
Robert Medina <robertmedib1@gmail.com>
2020-06-24 23:50:05 -0500

    User can sign up, login, logout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
-import {create, login} from "./robert.js";
+import { login, signout, createUser } from "./robert.js";
+const user = JSON.parse(localStorage.getItem("user"));
 
-const loginBtn = document.querySelector("#login-btn")
-const createBtn = document.querySelector("#create-btn")
+console.log(user);
+const loginBtn = document.querySelector("#login-btn");
+const createBtn = document.querySelector("#create-btn");
+const signoutBtn = document.querySelector("#sign-out");
 
-if (createBtn) createBtn.addEventListener("click", create);
+if (createBtn) createBtn.addEventListener("click", createUser);
 if (loginBtn) loginBtn.addEventListener("click", login);
+if (signoutBtn) signoutBtn.addEventListener("click", signout);
 
+export { user };
