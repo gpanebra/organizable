25f3abee30b62603e916d39c8e0be724255330f6
gpanebra <gpanebramoina@gmail.com>
2020-06-25 09:15:04 -0500

    update

---
+++ assets/js/gustavo.js
@@ -0,0 +1,11 @@
+/*var myBoardsButton = document.querySelector("my-boards-button");
+const closedBoardsButton = document.querySelector("closed-boards-button");
+const myProfileButton = document.querySelector("my-profile-button");
+const logOutButton = document.querySelector("log-out-button");*/
+
+/*function changeColor(element) {
+  element.style.color = "yellow";
+  element.stopPropagation();
+}
+*/
+//onclick="changeColor(this)"
